ab8717e5732d335e65664a967eb5d6c5f2885e71
owais3665 <133983497+owais3665@users.noreply.github.com>
2023-06-14 19:47:54 +0530

    Create cicd.groovy

---
+++ vars/cicd.groovy
@@ -0,0 +1,19 @@
+def contiDownload(repo)
+{  
+  git "https://github.com/intelliqittrainings/${repo}"
+}
+
+def newBuild()
+{
+  sh 'mvn package'
+}
+
+def contiDeployment(jobname,ip,context)
+{
+  sh "scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${context}.war"
+}
+
+def seliniumTest(jobname)
+{
+  sh "java -jar /var/lib/jenkins/workspace/${jobname}/testing.jar"
+}
